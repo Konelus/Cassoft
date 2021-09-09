e0dce9546243d37c51f2ffaf5a7e5b9efd7fb303
Konelus <31219413+Konelus@users.noreply.github.com>
2021-09-09 23:59:50 +0300

    Tiny fix

--- pages/posts.php
+++ pages/posts.php
@@ -17,7 +17,7 @@
             <?php if ($messages[0] != '') { foreach ($messages[0] as $key => $value) { if ($value['status'] == 1) { ?>
              <a href = '?page=message&id=<?= $value['id'] ?>'>
                 <div class = 'row mail-row'>
-                    <div class = 'col mail-title'><span style = 'color: <?= $value['color'] ?>;'>&#9733;</span> <?= $value['title'] ?></div>
+                    <div class = 'col-7 mail-title'><span style = 'color: <?= $value['color'] ?>;'>&#9733;</span> <?= $value['title'] ?></div>
                     <div class = 'col'><?= $value['name'] ?></div>
                 </div>
              </a>
@@ -32,7 +32,7 @@
             <?php if ($messages[0] != '') { foreach ($messages[0] as $key => $value) { if ($value['status'] == 0) { ?>
             <a href = '?page=message&id=<?= $value['id'] ?>'>
                 <div class = 'row mail-row'>
-                    <div class = 'col mail-title'><span style = 'color: <?= $value['color'] ?>;'>&#9733;</span> <?= $value['title'] ?></div>
+                    <div class = 'col-7 mail-title'><span style = 'color: <?= $value['color'] ?>;'>&#9733;</span> <?= $value['title'] ?></div>
                     <div class = 'col'><?= $value['name'] ?></div>
                 </div>
             </a>
